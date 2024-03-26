29f73c61410e83928cbaf75fdbc4058634a84aee
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-03-26 22:16:51 +0100

    Added ts-node

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      
-    </main>
+    <div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+    </div>
   );
 }

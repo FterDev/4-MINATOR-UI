7007e446efc252ff8790de588bd98930a0ef88af
Fedor Terekhov <devops@terekhov.ch>
2024-04-30 22:47:47 +0200

    Added sign up page

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -8,16 +8,16 @@ import FmLink from '@/app/components/ui/fmlink/fmlink';
 
 
 
-export default function SignIn()
+export default function SignUp()
 {
     return (
         <form>
-            <Auth text="Please login to start playing.">
+            <Auth text="Please enter the required information to create an account.">
+                <FmInput id="nickname" name="nickname" title="Nickname" value="" placeholder="FluffyUnicorn" type="text" textAlign="center" />
                 <FmInput id="email" name="email" title="E-Mail" value="" placeholder="fluffy@unicorn.com" type="email" textAlign="center" />
                 <FmInput id="password" name="password" title="Password" value="" placeholder="●●●●●●●" type="password" textAlign="center" />
-                <FmButton text="Sign In" className="signin-button" submmit onClick={() => {}} />
-                <FmLink text="Forgot your password?" href="/auth/forgot" className="signin-link"/>
-                <FmLink text="No account? Create one!" href="/auth/signup" className="signin-link"/>
+                <FmButton text="Sign Up" className="signin-button" submmit onClick={() => {}} />
+                <FmLink text="Back to Sign In" href="/auth/signin" className="signin-link"/>
             </Auth>
         </form>
     );

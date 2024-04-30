ac60e387f6b27a1aca9057d7d9b85840a550987c
Fedor Terekhov <devops@terekhov.ch>
2024-04-30 22:13:32 +0200

    Added sign in page

---
+++ app/auth/signup/page.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import './page.css';
+import Auth from "@/app/components/app/auth/auth";
+import FmButton from "@/app/components/ui/fmbutton/fmbutton";
+import FmInput from "@/app/components/ui/fminput/fminput";
+
+
+
+export default function SignUp()
+{
+    return (
+        <form>
+            <Auth text="Please login to start playing.">
+                <FmInput id="email" name="email" title="E-Mail" value="" placeholder="fluffy@unicorn.com" type="email" textAlign="center" />
+                <FmInput id="password" name="password" title="Password" value="" placeholder="●●●●●●●" type="password" textAlign="center" />
+                <FmButton text="Sign In" className="signin-button" onClick={() => {}} />
+            </Auth>
+        </form>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,7 @@ import FmInput from "./components/ui/fminput/fminput";
 export default function Home() {
   return (
     <div>
-      <Auth text="Please login to start playing.">
-        <FmInput id="email" name="email" title="E-Mail" value="" placeholder="fluffy@unicorn.com" type="email" textAlign="center" />
-        <FmInput id="password" name="password" title="Password" value="" placeholder="●●●●●●●" type="password" textAlign="center" />
-        <FmButton text="Sign In" onClick={() => {}} />
-      </Auth>
+      
     </div>
   );
 }

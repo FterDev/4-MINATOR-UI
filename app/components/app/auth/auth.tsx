bd9e43030ca02103ee24e798f8e2653fd8a57642
Fedor Terekhov <devops@terekhov.ch>
2024-04-30 21:08:37 +0200

    Added Auth Logo

--- app/components/app/auth/auth.tsx
+++ app/components/app/auth/auth.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './auth.css';
 import FmCard from '../../ui/fmcard/fmcard';
+import { Flex } from 'antd';
+import Image from 'next/image';
 
 
 interface AuthProps {
@@ -11,7 +13,9 @@ let Auth : React.FC<AuthProps> = ({ children }) => {
     return (
         <div className="auth">
             <FmCard className="auth-card">
-              
+                <Flex justify='center'>
+                    <Image src="/img/logo_transparent.png" alt="logo" width={0} height={0} sizes="100vw" className="auth-logo"/>
+                </Flex>    
             </FmCard>
         </div>
     );

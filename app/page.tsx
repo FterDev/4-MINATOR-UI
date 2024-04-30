96338d1b0ffe7fb5eb0c1c0dfdca725d11655ef3
Fedor Terekhov <devops@terekhov.ch>
2024-04-30 21:46:10 +0200

    Updated input border color

--- app/page.tsx
+++ app/page.tsx
@@ -7,8 +7,8 @@ export default function Home() {
   return (
     <div>
       <Auth text="Please login to start playing.">
-        <FmInput id="email" name="email" title="E-Mail" value="" placeholder="fluffy@unicorn.com" />
-        <FmInput id="password" name="password" title="Password" value="" type="password" />
+        <FmInput id="email" name="email" title="E-Mail" value="" placeholder="fluffy@unicorn.com" type="email" textAlign="center" />
+        <FmInput id="password" name="password" title="Password" value="" placeholder="●●●●●●●" type="password" textAlign="center" />
       </Auth>
     </div>
   );

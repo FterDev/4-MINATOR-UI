0fa8b0f7acff222d588b6d9e0d01863fd54c3813
Fedor Terekhov <devops@terekhov.ch>
2024-04-30 21:52:37 +0200

    Auth children added padding bottom

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
-
 import Auth from "./components/app/auth/auth";
+import FmButton from "./components/ui/fmbutton/fmbutton";
 import FmInput from "./components/ui/fminput/fminput";
 
 export default function Home() {
@@ -9,6 +9,7 @@ export default function Home() {
       <Auth text="Please login to start playing.">
         <FmInput id="email" name="email" title="E-Mail" value="" placeholder="fluffy@unicorn.com" type="email" textAlign="center" />
         <FmInput id="password" name="password" title="Password" value="" placeholder="●●●●●●●" type="password" textAlign="center" />
+        <FmButton text="Sign In" onClick={() => {}} />
       </Auth>
     </div>
   );

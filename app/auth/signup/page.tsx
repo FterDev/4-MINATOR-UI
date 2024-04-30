f49e6cf7a0de4b943f18979a6b198b6bd24e3443
Fedor Terekhov <devops@terekhov.ch>
2024-04-30 22:23:07 +0200

    Updated FmCard padding

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -14,7 +14,8 @@ export default function SignUp()
             <Auth text="Please login to start playing.">
                 <FmInput id="email" name="email" title="E-Mail" value="" placeholder="fluffy@unicorn.com" type="email" textAlign="center" />
                 <FmInput id="password" name="password" title="Password" value="" placeholder="●●●●●●●" type="password" textAlign="center" />
-                <FmButton text="Sign In" className="signin-button" onClick={() => {}} />
+                <FmButton text="Sign In" className="signin-button" submmit onClick={() => {}} />
+                
             </Auth>
         </form>
     );

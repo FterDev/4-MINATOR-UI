3bfe31e584c3a0b055f7a060fdc10249774b154a
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-01 14:06:34 +0000

    Added FmLoading component

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 'use client';
 
 import FmLink from "./components/ui/fmlink/fmlink";
+import FmLoading from "./components/ui/fmloading/fmloading";
 import FmMessage from "./components/ui/fmmessage/fmmessage";
 
 export default function Home() {
   return (
     <div>
-      <FmMessage message="Hello World! This is a very long message!" type="success">
-        <FmLink href="/" text="Back to Sign In" />
-      </FmMessage>
+      <FmLoading/>
     </div>
   );
 }

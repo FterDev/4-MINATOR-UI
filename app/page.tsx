471362d7327c8d1cf19f3868156986238e8783b8
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-01 12:15:39 +0000

    FmCard implemented

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
+import FmLink from "./components/ui/fmlink/fmlink";
 import FmMessage from "./components/ui/fmmessage/fmmessage";
 
 export default function Home() {
   return (
     <div>
-      <FmMessage message="Hello World! This is a very long message!" type="success" />
+      <FmMessage message="Hello World! This is a very long message!" type="success">
+        <FmLink href="/" text="Back to Sign In" />
+      </FmMessage>
     </div>
   );
 }

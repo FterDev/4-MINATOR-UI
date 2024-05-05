8d8debef72c9fb10548b817f45325b4e7deeb384
Fedor Terekhov <fedor@terekhov.ch>
2024-05-05 15:28:45 +0200

    Fixed setting errors on Sign in fields

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -49,8 +49,8 @@ export default function SignIn()
     return (
         <form onSubmit={handleSignIn}>
             <Auth text="Please login to start playing.">
-                <FmInput id="email" name="email" title="E-Mail" value={email} onBlur={(e) => emailValidation.validateEmail(e)} onChange={(e) => setEmail(e)} isErrored={emailError.isErrored} errorText={emailError.errorText} placeholder="fluffy@unicorn.com" type="email" textAlign="center" />
-                <FmInput id="password" name="password" title="Password" value={password} onBlur={(e) => passwordValidation.validateOnlyEmpty(e)} onChange={(e) => setPassword(e)} isErrored={passwordError.isErrored} errorText={passwordError.errorText} placeholder="●●●●●●●" type="password" textAlign="center" />
+                <FmInput id="email" name="email" title="E-Mail" value={email} onBlur={(e) => setEmailError(emailValidation.validateEmail(e))} onChange={(e) => setEmail(e)} isErrored={emailError.isErrored} errorText={emailError.errorText} placeholder="fluffy@unicorn.com" type="email" textAlign="center" />
+                <FmInput id="password" name="password" title="Password" value={password} onBlur={(e) => setPasswordError(passwordValidation.validateOnlyEmpty(e))} onChange={(e) => setPassword(e)} isErrored={passwordError.isErrored} errorText={passwordError.errorText} placeholder="●●●●●●●" type="password" textAlign="center" />
                 <FmButton text="Sign In" className="signin-button" submmit />
                 <FmLink text="Forgot your password?" href="/auth/forgot" className="signin-link"/>
                 <FmLink text="No account? Create one!" href="/auth/signup" className="signin-link"/>

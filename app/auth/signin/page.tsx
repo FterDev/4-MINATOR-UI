6d72b42da5e8c90cd2cdfe1a0eb4d68ee7b63929
Fedor Terekhov <fedor@terekhov.ch>
2024-05-05 15:27:41 +0200

    Sign in assigned all error states to inputs

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -18,7 +18,6 @@ export default function SignIn()
 
 
     const [error, setError] = useState<Boolean>(true);
-    const [success, setSuccess] = useState<Boolean>(false);
     const [emailError , setEmailError] = useState<ValidationErrorResponse>(
         {
             errorText: null,
@@ -37,15 +36,22 @@ export default function SignIn()
     const [password, setPassword] = useState<string>("");
 
     const emailValidation = new EmailValidationProvider();
-    const passwordValidation = new PasswordValidationProvider();
+    const passwordValidation = new PasswordValidationProvider(null);
     const auth0service = new Auth0Service();
 
+
+    async function handleSignIn(event: React.FormEvent<HTMLFormElement>) {
+    
+
+    
+    }
+
     return (
-        <form>
+        <form onSubmit={handleSignIn}>
             <Auth text="Please login to start playing.">
-                <FmInput id="email" name="email" title="E-Mail" value={email} placeholder="fluffy@unicorn.com" type="email" textAlign="center" />
-                <FmInput id="password" name="password" title="Password" value={password} onBlur={} placeholder="●●●●●●●" type="password" textAlign="center" />
-                <FmButton text="Sign In" className="signin-button" submmit onClick={() => {}} />
+                <FmInput id="email" name="email" title="E-Mail" value={email} onBlur={(e) => emailValidation.validateEmail(e)} onChange={(e) => setEmail(e)} isErrored={emailError.isErrored} errorText={emailError.errorText} placeholder="fluffy@unicorn.com" type="email" textAlign="center" />
+                <FmInput id="password" name="password" title="Password" value={password} onBlur={(e) => passwordValidation.validateOnlyEmpty(e)} onChange={(e) => setPassword(e)} isErrored={passwordError.isErrored} errorText={passwordError.errorText} placeholder="●●●●●●●" type="password" textAlign="center" />
+                <FmButton text="Sign In" className="signin-button" submmit />
                 <FmLink text="Forgot your password?" href="/auth/forgot" className="signin-link"/>
                 <FmLink text="No account? Create one!" href="/auth/signup" className="signin-link"/>
             </Auth>

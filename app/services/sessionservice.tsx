f9e61649a91e16cafc0e332ea1416953b0c1bce6
Fedor Terekhov <fedor@terekhov.ch>
2024-05-07 22:58:28 +0200

    Added crypto js

--- app/services/sessionservice.tsx
+++ app/services/sessionservice.tsx
@@ -3,6 +3,16 @@ import { cookies } from 'next/headers';
 
 
 
+function encryptToken(token:string)
+{
+    return token;
+}
+
+function decryptToken(token:string)
+{
+    return token;
+}
+
 
 export async function createSession(token:string, id:string, expiresIn:number) {
 
@@ -13,7 +23,7 @@ export async function createSession(token:string, id:string, expiresIn:number) {
     }
     const expiresAt = new Date().getTime() + expiresIn * 1000;
 
-    cookies().set('session', JSON.stringify(session), {
+    cookies().set('session/token', JSON.stringify(session), {
         httpOnly: true,
         secure: true,
         expires: expiresAt,

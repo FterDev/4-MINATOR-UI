f4468b9bd4f27b6f189a3c520a3d27c86a1050e2
FTE <devops@terekhov.ch>
2024-05-08 22:33:50 +0200

    Added FmNavigation files

---
+++ app/components/ui/fmnavigation/fmnavigation.tsx
@@ -0,0 +1,17 @@
+import './fmnavigation.css';
+import FmCard from "../fmcard/fmcard";
+
+
+
+
+
+export default function FmNavigation() {
+
+
+    return (
+        <FmCard>
+            test
+        </FmCard>
+    );
+
+}

a8af414e850901eab2cf6196c8ab5710418cc706
FTE <devops@terekhov.ch>
2024-05-08 22:23:15 +0200

    Added get request for userdata

--- app/api/auth/route.tsx
+++ app/api/auth/route.tsx
@@ -1,7 +1,8 @@
 
 import { createSession } from "@/app/services/sessionservice";
 import { NextResponse } from "next/server";
-import { SignIn } from "@/app/services/auth0service";
+import { SetSessionData, SignIn } from "@/app/services/auth0service";
+import { cookies } from "next/headers";
 
 
 
@@ -20,4 +21,15 @@ export async function POST(request: Request) {
     createSession(token, id, expiresIn);
     return NextResponse.json({message: "Success", status: 200})
 
+}
+
+
+export async function GET() {
+    
+    const session = JSON.parse(cookies().get('session')?.value.toString() || '{}')
+    if (session.token){
+        const res = await SetSessionData(session.token);
+        return NextResponse.json(res, {status: 200});
+    }
+    return NextResponse.json({message: "Unauthorized", status: 401})
 }

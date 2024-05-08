949cc94aff57efe2780e24a3680893e892c14a3c
FTE <devops@terekhov.ch>
2024-05-08 23:03:08 +0200

    Added loading and fetch to nav

--- app/game/nav/page.tsx
+++ app/game/nav/page.tsx
@@ -3,6 +3,9 @@
 import { useSelector, useDispatch } from "react-redux";
 import { setUsername, setGravatar } from "@/app/states/userDataSlice";
 import FmNavigation from "@/app/components/ui/fmnavigation/fmnavigation";
+import { use, useEffect, useState } from "react";
+import FmLoading from "@/app/components/ui/fmloading/fmloading";
+
 
 
 
@@ -17,15 +20,28 @@ export default function Nav() {
     const userData = useSelector((state: any) => state.userData);
     const dispatch = useDispatch();
 
-    if (!userData.username) {
-        fetch('/api/auth', { method: 'GET' }).then(res => res.json()).then(data => {
-            dispatch(setUsername(data.nickname));
-            dispatch(setGravatar(data.picture));
-        });
-    }
+    const [loading, setLoading] = useState<boolean>(true);
+
+    
+    
+    useEffect(() => {
+        fetch("/api/auth")
+            .then((res) => res.json())
+            .then((data) => {
+                dispatch(setUsername(data.nickname));
+                dispatch(setGravatar(data.picture));
+                setLoading(false);
+            });
+    
+    }, []);
+    
+    if(loading) return <FmLoading />;
+
+ 
 
 
     return (
+      
         <FmNavigation username={userData.username} picture={userData.gravatar} />
     );
 }

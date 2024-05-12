f8abbc80c1869d526b0c6aa4df88a4c2e784ff7c
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-12 10:07:32 +0200

    Added Firebase config

---
+++ app/firebase.ts
@@ -0,0 +1,18 @@
+
+import { getApp, getApps, initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FB_APIKEY,
+  authDomain: process.env.NEXT_PUBLIC_FB_AUTHDOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FB_PROJECTID,
+  storageBucket: process.env.NEXT_PUBLIC_FB_STORAGE,
+  messagingSenderId: process.env.NEXT_PUBLIC_FB_MESSAGINGSENDERID,
+  appId: process.env.NEXT_PUBLIC_FB_APPID,
+  measurementId: process.env.NEXT_PUBLIC_FB_MEASUREMENTID
+};
+
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth, app };

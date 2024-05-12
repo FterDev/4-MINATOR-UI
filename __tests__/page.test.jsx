cb46caf8913288654057044323c6f1eaf2397290
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-12 12:52:35 +0200

    Fixed rest of tests

--- __tests__/page.test.jsx
+++ __tests__/page.test.jsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import { render } from '@testing-library/react'
-import Page from '../app/page'
+import Page from '../src/app/page'
  
 describe('Page', () => {
   it("should create an HTMLDivElement", async () => {

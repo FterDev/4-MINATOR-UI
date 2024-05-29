df829ad388b3044a655a99f0e14a674df54890e9
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-05-29 09:52:09 +0200

    Added Lobby element to page

---
+++ src/app/game/lobby/page.tsx
@@ -0,0 +1,14 @@
+import FmLobby from "@/app/components/ui/fmlobby/fmlobby";
+
+ 
+
+
+
+export default function Lobby()
+{
+    return (
+        <FmLobby>
+            
+        </FmLobby>
+    )
+}

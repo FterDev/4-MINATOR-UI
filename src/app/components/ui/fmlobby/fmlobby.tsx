bf005ed8531d501ba9de7595462b52985ec24f5a
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-06-11 17:08:52 +0200

    Moved all data to component

--- src/app/components/ui/fmlobby/fmlobby.tsx
+++ src/app/components/ui/fmlobby/fmlobby.tsx
@@ -6,55 +6,168 @@ import FmButton from "../fmbutton/fmbutton";
 import FmTable from "../fmtable/fmtable";
 import { useRouter } from "next/navigation";
 import { use, useEffect, useState } from "react";
-import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
-import { useSelector } from "react-redux";
+import { HubConnection, HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
+import { useDispatch, useSelector } from "react-redux";
 import { FmLobbyCell } from "../fmlobbycell/fmlobbycell";
+import { setMatchId } from "@/app/slices/sessionSlice";
+import FmNotification from "../fmnotification/fmnotification";
+import FmAwaitingResponse from "../fmawaitingresponse/fmawaitingresponse";
+import FmLoading from "../fmloading/fmloading";
+import { FmModal } from "../fmmodal/fmmodal";
+import { LoadingOutlined } from "@ant-design/icons";
 
-
-
-interface FmLobbyProps {
-
-    data: any;
-    requestMatch: (playerId: number) => void;
-}
-
-
-export default function FmLobby(data: FmLobbyProps)
+export default function FmLobby()
 {
 
-    const [players, setPlayers] = useState<any>([]);
+    
+    
+
+    const sessionData = useSelector((state: any) => state.session);
+
+    const [players, setPlayers] = useState<any>();
+    const [connection, setConnection] = useState<HubConnection | null>(null);
+    const [waitingResponse, setWaitingResponse] = useState<boolean>(false);
+    const [requestingMatch, setRequestingMatch] = useState<boolean>(false);
+    const [modal , setModal] = useState<boolean>(false);
+    const [botLevelWindow, setBotLevelWindow] = useState<boolean>(false);
+
+    const dispatch = useDispatch();
     const router = useRouter();
-    
-    const playersArray =  Array.from(data.data);
+    const [loading, setLoading] = useState<boolean>(true);
 
-    
-
-
-
-    const columns = [
-        { key: 'row', title: 'Available Players' },    
-    ];
+    const [matchData, setMatchData] = useState<any>(null);
+    const [requester, setRequester] = useState<any>(null);
+    const [targetPlayer, setTargetPlayer] = useState<any>(null);
 
 
+    const backendUrl = process.env.NEXT_PUBLIC_LINK_BACKEND;
+
+
+    async function requestMatch(playerId: number)
+    {
+        connection?.invoke("RequestMatch", playerId).catch((err) => console.log(err));
+    }
+
+    async function cancelMatch()
+    {
+        connection?.invoke("CancelMatch", matchData.id).catch((err) => console.log(err));
+    }
+
+    async function acceptMatch()
+    {
+        connection!.invoke("AcceptMatch", matchData.id).catch((err) => console.log(err));
+    }
+
+    function openBotLevelWindow()
+    {
+        setBotLevelWindow(true);
+    }
+
+
+    useEffect(() => {
+
+        const connect = new HubConnectionBuilder()
+            .withUrl(backendUrl + "/lobbyHub", {skipNegotiation: false, transport: 1, accessTokenFactory: () => sessionData.token 
+            })
+            .configureLogging(LogLevel.Information)
+            .withAutomaticReconnect()
+            .build();
+
+        setConnection(connect);
+
+        connect.start().then(() => { 
+            
+            connect.on("ReceiveWaitingPlayers", (res) => {
+                console.log(res);
+                setPlayers(res);
+                setLoading(false);
+            });
+
+            connect.on("ReceiveMatchRequest", (res) => {
+                setRequester(res);
+                setModal(true);
+                setRequestingMatch(true);
+            });
+
+            connect.on("ReceiveTargetUser", (res) => {
+                setWaitingResponse(true);
+                setModal(true);
+                setTargetPlayer(res);
+                
+            });
+
+            connect.on("ReceivePendingMatch", (res) => {
+                setMatchData(res);
+            });
+
+            connect.on("ReceiveMatchCanceled", () => {
+                setModal(false);
+                setWaitingResponse(false);
+                setRequestingMatch(false);
+                setMatchData(null);
+            });
+
+            connect.on("ReceiveMatchAccepted", (res) => {
+                console.log(res);
+                dispatch(setMatchId(res));
+                connection?.stop();
+                router.push('/game/gm/' + res);
+            });
+
+
+            
+        }).catch((err) => console.log(err));
+
+        return () => {
+            if (connection) {
+                connection.off("ReceiveWaitingPlayers");
+                connection.off("ReceiveMatchRequest");
+                connection.off("ReceiveTargetUser");
+                connection.off("ReceivePendingMatch");
+                connection.off("ReceiveMatchCanceled");
+                connection.off("ReceiveMatchAccepted");
+                connection.stop();
+            }
+        }
+        
+    }, []);
     
 
 
     return(
-        <FmCard className="fm-lobby-card">
-            <Flex className="fm-lobby-options">
-                <FmButton text="Back" className="fm-lobby-button" onClick={()=>{router.push('/game/nav')}}/>
-            </Flex>
-            <Flex className="fm-lobby-title" justify="center">
-                <h1>Available Players:</h1>
-            </Flex>
-            <div className="fm-lobby-table-wrapper">
-                {playersArray.map((player: any) => {
-                    return <FmLobbyCell externalId={player.user.externalId} nickname={player.user.nickname} playerId={player.id} request={data.requestMatch} />
-                })}
-            </div>
-            
-            
-        </FmCard>
+        <> 
+        <Flex justify="center">
+            <FmCard className="fm-lobby-card">
+                <Flex className="fm-lobby-options">
+                    <FmButton text="Back" className="fm-lobby-button" onClick={()=>{router.push('/game/nav')}}/>
+                </Flex>
+                <Flex className="fm-lobby-title" justify="center">
+                    <h1>Available Bots:</h1>
+                </Flex>
+                <div className="fm-lobby-table-wrapper">
+                    <FmLobbyCell externalId='dyeh9EByFNfR8qqQEqTz92af0kQ2' nickname='T-800' playerId={800} request={openBotLevelWindow} />
+                </div>
+                <Flex className="fm-lobby-title" justify="center">
+                    <h1>Available Players:</h1>
+                </Flex>
+                <div className="fm-lobby-table-wrapper">
+                    {players == undefined ? <LoadingOutlined /> :
+                    players.map((player: any) => {
+                        console.log(player);
+                        return <FmLobbyCell externalId={player.user.externalId} nickname={player.user.nickname} playerId={player.id} request={requestMatch} />
+                    })}
+                </div> 
+            </FmCard>
+        </Flex>
+           
         
+        <FmNotification show={modal}>
+            {waitingResponse && <FmAwaitingResponse playerNickname={targetPlayer.nickname} playerExternalId={targetPlayer.externalId} awaitingResponse cancelMethod={cancelMatch} acceptMethod={ () => {}} ></FmAwaitingResponse>}
+            {requestingMatch && <FmAwaitingResponse playerNickname={requester.nickname} playerExternalId={requester.externalId} awaitingResponse={false} cancelMethod={cancelMatch} acceptMethod={acceptMatch}></FmAwaitingResponse>}
+        </FmNotification>
+        <FmModal visible={botLevelWindow} textOk='Play' onCancel={()=>{setBotLevelWindow(false)}} onOk={() => {}}>
+            hello
+        </FmModal>
+        </>
     )
 }
--- src/app/game/lobby/page.tsx
+++ src/app/game/lobby/page.tsx
@@ -1,13 +1,5 @@
 'use client';
-import FmAwaitingResponse from "@/app/components/ui/fmawaitingresponse/fmawaitingresponse";
 import FmLobby from "@/app/components/ui/fmlobby/fmlobby";
-import FmNotification from "@/app/components/ui/fmnotification/fmnotification";
-import { setMatchId } from "@/app/slices/sessionSlice";
-import { HubConnection, HubConnectionBuilder, LogLevel } from "@microsoft/signalr";
-import { useRouter } from "next/navigation";
-
-import { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 
  
 
@@ -17,118 +9,10 @@ export default function Lobby()
 {
 
 
-    const sessionData = useSelector((state: any) => state.session);
-
-    const [players, setPlayers] = useState<String>('');
-    const [connection, setConnection] = useState<HubConnection | null>(null);
-    const [waitingResponse, setWaitingResponse] = useState<boolean>(false);
-    const [requestingMatch, setRequestingMatch] = useState<boolean>(false);
-    const [modal , setModal] = useState<boolean>(false);
-
-    const dispatch = useDispatch();
-    const router = useRouter();
-
-    const [matchData, setMatchData] = useState<any>(null);
-    const [requester, setRequester] = useState<any>(null);
-    const [targetPlayer, setTargetPlayer] = useState<any>(null);
-
-    const backendUrl = process.env.NEXT_PUBLIC_LINK_BACKEND;
-
-
-    async function requestMatch(playerId: number)
-    {
-        connection?.invoke("RequestMatch", playerId).catch((err) => console.log(err));
-    }
-
-    async function cancelMatch()
-    {
-        connection?.invoke("CancelMatch", matchData.id).catch((err) => console.log(err));
-    }
-
-    async function acceptMatch()
-    {
-        connection?.invoke("AcceptMatch", matchData.id).catch((err) => console.log(err));
-    }
-
-
-    useEffect(() => {
-
-        const connect = new HubConnectionBuilder()
-            .withUrl(backendUrl + "/lobbyHub", {skipNegotiation: false, transport: 1, accessTokenFactory: () => sessionData.token 
-            })
-            .configureLogging(LogLevel.Information)
-            .withAutomaticReconnect()
-            .build();
-
-        setConnection(connect);
-
-        connect.start().then(() => { 
-            
-            connect.on("ReceiveWaitingPlayers", (res) => {
-                setPlayers(res);
-            });
-
-            connect.on("ReceiveMatchRequest", (res) => {
-                setRequester(res);
-                setModal(true);
-                setRequestingMatch(true);
-            });
-
-            connect.on("ReceiveTargetUser", (res) => {
-                setWaitingResponse(true);
-                setModal(true);
-                setTargetPlayer(res);
-                
-            });
-
-            connect.on("ReceivePendingMatch", (res) => {
-                setMatchData(res);
-            });
-
-            connect.on("ReceiveMatchCanceled", () => {
-                setModal(false);
-                setWaitingResponse(false);
-                setRequestingMatch(false);
-                setMatchData(null);
-            });
-
-            connect.on("ReceiveMatchAccepted", (res) => {
-                console.log(res);
-                dispatch(setMatchId(res));
-                connection?.stop();
-                router.push('/game/gm/' + res);
-            });
-
-
-            
-        }).catch((err) => console.log(err));
-
-        return () => {
-            if (connection) {
-                connection.off("ReceiveWaitingPlayers");
-                connection.off("ReceiveMatchRequest");
-                connection.off("ReceiveTargetUser");
-                connection.off("ReceivePendingMatch");
-                connection.off("ReceiveMatchCanceled");
-                connection.off("ReceiveMatchAccepted");
-                connection.stop();
-            }
-        }
-        
-    }, []);
+   
 
     
     return (
-        <>
-        <FmLobby data={players} requestMatch={requestMatch} />  
-        <FmNotification show={modal}>
-            {waitingResponse && <FmAwaitingResponse playerNickname={targetPlayer.nickname} playerExternalId={targetPlayer.externalId} awaitingResponse cancelMethod={cancelMatch} acceptMethod={ () => {}} ></FmAwaitingResponse>}
-            {requestingMatch && <FmAwaitingResponse playerNickname={requester.nickname} playerExternalId={requester.externalId} awaitingResponse={false} cancelMethod={cancelMatch} acceptMethod={acceptMatch}></FmAwaitingResponse>}
-        </FmNotification>
-        </>
-        
+        <FmLobby />    
     )
-    
-         
-    
 }

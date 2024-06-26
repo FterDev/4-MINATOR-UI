3b6a2a612ac8201a2acfc474f2a20b5b5842f754
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-06-26 10:21:53 +0200

    Added RobotForm Compoment Base

---
+++ src/app/components/app/robots/robotform/robotform.tsx
@@ -0,0 +1,12 @@
+import FmCard from '@/app/components/ui/fmcard/fmcard';
+import './robotform.css';
+
+
+export default function RobotForm()
+{
+    return (
+        <FmCard>
+            <h1>Robot Form</h1>
+        </FmCard>
+    );
+}
---
+++ src/app/game/robots/[robot]/page.tsx
@@ -0,0 +1,14 @@
+
+
+
+
+
+
+export default function Robot()
+{
+    return (
+        <div>
+            <h1>Robot</h1>
+        </div>
+    );
+}

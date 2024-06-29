621cd8a22a0a74b932b87bb94f9fc02742d140dc
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-06-29 16:28:58 +0200

    Added Timer Component

---
+++ src/app/components/ui/fmtimer/fmtimer.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { useEffect, useState } from "react";
+import './fmtimer.css';
+
+
+
+
+interface IFmTimerProps {
+    targetTime: Date;
+}
+
+
+
+export const FmTimer: React.FC<IFmTimerProps> = (props) => {
+    const [timeLeft, setTimeLeft] = useState<number>(0);
+    const [timeLeftString, setTimeLeftString] = useState<string>("");
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            const now = new Date().getTime();
+            const distance = props.targetTime.getTime() - now;
+
+            setTimeLeft(distance);
+            setTimeLeftString(new Date(distance).toISOString().substr(14, 5));
+        }, 1000);
+
+        return () => clearInterval(interval);
+    }, [props.targetTime]);
+
+    return (
+        <div>
+            {timeLeftString}
+        </div>
+    );
+};
+

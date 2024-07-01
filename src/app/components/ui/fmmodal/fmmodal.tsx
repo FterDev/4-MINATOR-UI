3eb671bdfd35a82935987ceb02857832ed4437f3
Fedor Terekhov <51990729+FterDev@users.noreply.github.com>
2024-07-01 16:39:16 +0200

    Added possibility to hide buttons

--- src/app/components/ui/fmmodal/fmmodal.tsx
+++ src/app/components/ui/fmmodal/fmmodal.tsx
@@ -13,16 +13,18 @@ interface FmModalProps {
     onCancel: () => void;
     textOk?: string;
     textCancel?: string;
+    hideCancel?: boolean;
+    hideOk?: boolean;
 }
 
-export const FmModal: React.FC<FmModalProps> = ({visible, onOk, onCancel, children, textOk, textCancel}) => {
+export const FmModal: React.FC<FmModalProps> = ({visible, onOk, onCancel, children, textOk, textCancel, hideCancel = false, hideOk = false}) => {
     return (
         <Flex justify="center" align="center" className={`fm-modal ${visible?'visible':''}`}>
             <FmCard className="fm-modal-card">
                 {children}
                 <Flex justify="center" align="center" className="fm-modal-buttons">
-                    <FmButton text={textCancel != undefined ? textCancel : 'Cancel'} color="danger" onClick={onCancel} />
-                    <FmButton text={textOk != undefined ? textOk : 'Ok'} onClick={onOk} />  
+                    <FmButton className={hideCancel ? 'hide' : ''} text={textCancel != undefined ? textCancel : 'Cancel'} color="danger" onClick={onCancel} />
+                    <FmButton className={hideOk ? 'hide' : ''}  text={textOk != undefined ? textOk : 'Ok'} onClick={onOk} />  
                 </Flex>
             </FmCard>
         </Flex>
